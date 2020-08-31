cc1e1ce0197ff1fb3df79011620d1ce5c839e6a3
ichabot <5778207+ichabot@users.noreply.github.com>
2020-08-31 11:56:17 +0200

    Update snipeit.sh
    Install in Ubuntu 18.04 does not work when -ge is used. == fixed the Problem

--- snipeit.sh
+++ snipeit.sh
@@ -422,7 +422,7 @@ case $distro in
   fi
   ;;
   ubuntu)
-  if [ "$version" -ge "18.04" ]; then
+  if [ "$version" == "18.04" ]; then
     # Install for Ubuntu 18.04
     tzone=$(cat /etc/timezone)
 
